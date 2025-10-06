868e5a3cf218c673079450ad618df7773ecb8488
Zayne <68727794+zaynedevv@users.noreply.github.com>
2025-10-07 08:14:08 +1000

    Update server.py

--- server.py
+++ server.py
@@ -168,9 +168,9 @@ async def populate(
             file = await populate_file_async(
                 ILAName,
                 ILAData,
-                f'17. Individual Legal Advice Certificate - {director["GUARANTORNAME"]}'
+                f'17. Guarantor Legal Advice Warranty - {director["GUARANTORNAME"]}'
             )
-            files.append((f'17. Individual Legal Advice Certificate - {director["GUARANTORNAME"]}.zip', file))
+            files.append((f'17. Guarantor Legal Advice Warranty - {director["GUARANTORNAME"]}.zip', file))
 
         
     zip_buffer = io.BytesIO()
