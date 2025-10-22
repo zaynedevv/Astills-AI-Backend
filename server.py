c903cc6c7aae9aabc39df752605e53d5fb810779
Zayne <68727794+zaynedevv@users.noreply.github.com>
2025-10-22 11:46:01 +1000

    Update server.py

--- server.py
+++ server.py
@@ -172,6 +172,51 @@ async def populate(
             )
             files.append((f'12. Guarantor Legal Advice Warranty - {director["GUARANTORNAME"]}.zip', file))
 
+    if lender == "Source" and transaction_type == "Purchase" and matter_info["propery_state"] != "NSW":
+        ILAName = "SMSF/Purchase/Source/12. Solicitors Certificate Guarantor.docx"
+        template = await get_templates_async([ILAName])
+        print(template);
+    
+        for director in matter_info["directors"]:
+            ILAData = {}
+            for key in template[ILAName]:
+                if data.get(key):  # safer than data[key]
+                    ILAData[key] = data[key]
+                else:
+                    ILAData[key] = director.get(key)
+            print(ILAData)
+    
+            file = await populate_file_async(
+                ILAName,
+                ILAData,
+                f'12. Solicitors Certificate Guarantor - {director["GUARANTORNAME"]}'
+            )
+            files.append((f'12. Solicitors Certificate Guarantor - {director["GUARANTORNAME"]}.zip', file))
+
+     if lender == "Source" and transaction_type == "Purchase" and matter_info["propery_state"] == "NSW":
+        ILAName = "SMSF/Purchase/Source/12. Legal Advice Declaration - NSW.docx.docx"
+        template = await get_templates_async([ILAName])
+        print(template);
+    
+        for director in matter_info["directors"]:
+            ILAData = {}
+            for key in template[ILAName]:
+                if data.get(key):  # safer than data[key]
+                    ILAData[key] = data[key]
+                else:
+                    ILAData[key] = director.get(key)
+            print(ILAData)
+    
+            file = await populate_file_async(
+                ILAName,
+                ILAData,
+                f'12. Legal Advice Declaration - NSW - {director["GUARANTORNAME"]}'
+            )
+            files.append((f'12. Legal Advice Declaration - NSW - {director["GUARANTORNAME"]}.zip', file))
+
+
+    
+
         
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
